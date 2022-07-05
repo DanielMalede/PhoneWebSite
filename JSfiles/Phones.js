2f5bd0162af455a9266c32d84d841bc8c9554a8c
DanielMalede <danielmalede@gmail.com>
2022-07-05 17:45:32 +0300

    .

--- JSfiles/Phones.js
+++ JSfiles/Phones.js
@@ -1,40 +1,53 @@
 const API = `https://my-json-server.typicode.com/Jeck99/fake-server/devices`;
-let container = document.getElementById('container')
-let FaceImage = [
-    
-]
+let container = document.getElementById("container");
+let FaceImage = [];
 async function getUsers() {
   try {
-    return await fetch(API)
-    .then((response) =>
-    response.json() );
-    ;
-  } catch (error) {}
+    loadingGif.innerHTML = `    <img src="../images/GifLoad/loading.gif" alt="">`;
+    return await fetch(API).then((response) => response.json());
+  } catch (error) {
+  } finally {
+    loadingGif.innerHTML = ``;
+  }
 }
-console.log(getUsers()); 
+let phonesImageArray = [
+    '1phone.webp',
+    '2phone.webp',
+    '3phone.webp',
+    '4phone.webp',
+    '5phone.webp',
+    '6phone.webp',
+    '7phone.webp',
+    '8phone.webp',
+    '9phone.webp',
+    '10phone.webp',
+    '11phone.webp',
+    '12phone.webp',
+    '13phone.webp',
+    '14phone.webp',
+    '15phone.webp',
+]
 function printusersinfo() {
-    getUsers()
-    .then((result) =>{
-        result.forEach(item => {
-            container.innerHTML += `
-            <div class="col-md-4 px-3 pb-5 ">
-                <div class="card w-100" >
-                <img src=../images/StuffImage/Effie.jpg style="height:30vh ;>
-                    <div class="card-body">
-                    <p>${item.brad}</p>
-                    <p>${item.color}</p>
-                    <p>${item.isAvailable}</p>
-                    <p>${item.picture}</p>
-                    <p>${item.price}</p>
-                    <p>${item.email}</p>
-                    <p>${item.phone}</p>
-                    <br><br>
-                    </div>
-                </div>
+  getUsers().then((result) => {
+    result.forEach((item,index) => {
+      container.innerHTML += `
+            <div class=" px-3 pb-5 ">
+            <div class="bg-dark col card w-100" >
+            <img class="img-fluid" src="../images/PhonesPhotos/${phonesImageArray[index]}">
+            <div class="card-body text-white">
+            <p>${item.picture}</p>
+            <p><b>Brand</b> : ${item.brand}</p>
+            <p><b>Color</b> : ${item.color}</p>
+            <p><b>Ram</b> : ${item.ram}</p>
+            <p><b>Realse Date</b> : ${item.createdAt}</p>
+            <p><b>Price</b> : ${item.price}</p>
+            <p><b>In Stores</b> : ${item.isAvailable}</p>
+            <br><br>
             </div>
-            `
-        });
-    })
-    
+            </div>
+            </div>
+            `;
+    });
+  });
 }
-printusersinfo()
+printusersinfo();
--- JSfiles/Stuff.js
+++ JSfiles/Stuff.js
@@ -1,9 +1,5 @@
 const API = `https://my-json-server.typicode.com/Jeck99/fake-server/users`;
 let container = document.getElementById("mytable");
-let FaceImage = [
-  `../images/StuffImage/Terry.png`,
-  `../images/StuffImage/Terry.png`,
-];
 async function getUsers() {
   try {
     return await fetch(API).then((response) => response.json());
@@ -14,28 +10,47 @@ function printusersinfo() {
   getUsers().then((result) => {
     result.forEach((item) => {
       container.innerHTML += 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-                        `<tr>
-                        <td>${item.name.first} : ${item.name.last}</td> 
-                        <td>${item.age}</td>
-                        <td>${item.email}</td>
-                        <td>${item.phone}</td>
-                    </tr>`;
+        `<tr>
+        <td>${item.name.first} : ${item.name.last}</td> 
+        <td>${item.age}</td>
+        <td>${item.phone}</td>
+        </tr>`;
     });
   });
 }
 printusersinfo();
+
+
+async function addUser(){
+  let addUserInfo = {
+      info: {
+        age: AgeUser.value,
+        name :{
+          last:LastNameUser.value,
+          first:FirstNameUser.value,
+        },
+        email: EmailUser.value,
+        phone: PhoneUser.value,
+        picture: PictureUser.value,
+      }
+  }
+  try {
+    await fetch(`${API}`,
+    {
+      method: "post",
+      body: JSON.stringify(addUserInfo),
+      headers: {
+        'Content-Type': 'application/json'
+    }
+    })
+  } catch (error) {
+  }
+  finally{
+    container.innerHTML += `<tr>
+    <td>${AgeUser.value} : ${LastNameUser.value}</td> 
+    <td>${EmailUser.value}</td>
+    <td>${PhoneUser.value}</td>
+    <td>${PictureUser.value}</td>
+    </tr>`
+  }
+}
