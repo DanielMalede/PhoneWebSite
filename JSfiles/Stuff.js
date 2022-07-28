21cdf2e63c24aa31c22e4c01ad4c2e369ca70b07
DanielMalede <danielmalede@gmail.com>
2022-07-28 18:12:39 +0300

    .

--- JSfiles/Stuff.js
+++ JSfiles/Stuff.js
@@ -9,25 +9,7 @@ async function getUsers() {
     loding.innerHTML = "";
   }
 }
-// function printusersinfo() {
-//   getUsers().then((result) => {
-//     result.forEach((item,index) => {
-//       MyTableInStaff.innerHTML += `<tr class="">
-//       <td>${item.name.first}</td>
-//       <td>${item.name.last}</td>
-//       <td>${item.age}</td>
-//       <td>${item.email}</td>
-//       <td>${item.phone}</td>
-//       <td><img style="box-shadow: 0px 8px 10px black;" src="https://randomuser.me/api/portraits/med/men/${index}.jpg"></td>
-//       </tr>`;
-//     });
-//   });
-// }
-
 function printusersinfo() {
-  // const apiArr = await fetch(API).then((res) => res.json());
-  // let finallArr = [...apiArr, ...inputArr];
-  // console.log(inputArr)
   getUsers().then((response) =>
     response.forEach((item, index) => {
       MyTableInStaff.innerHTML += `<tr>
